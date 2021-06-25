5d22610070267047d17775314349f86fe2b8b146
felipe-costa-ft <felipecosta@ieee.org>
2021-06-24 23:33:14 -0300

    Challenge 1000 - 1001

---
+++ 1001.js
@@ -0,0 +1,9 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+let a = parseInt(lines.shift());
+let b = parseInt(lines.shift());
+
+let x = a + b;
+
+console.log(`"X = ${x}`);
---
+++ README.md
@@ -0,0 +1,1 @@
+Problems solutions for URI Online Judge
